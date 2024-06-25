1ec93a03da3a36d27daee6c6e19914401d685730
Matt Alioto <matthew.j.alioto@gmail.com>
2024-06-25 19:04:42 -0400

    Black, ruff

--- app2.py
+++ app2.py
@@ -5,7 +5,7 @@ import jwt
 
 st.header("Hello App2")
 
-# Header 
+# Header
 headers = _get_websocket_headers()
 
 st.write("Request headers:")
@@ -16,21 +16,21 @@ access_token = headers.get("X-Access-Token")
 
 sign_out_url_oauth_proxy = "http://localhost/oauth2/sign_out"
 # Must be set as a valid post redirect URI in your Keycloak client settings for your realm
-sign_out_url_keycloak = "http://localhost:8080/realms/myrealm/protocol/openid-connect/logout"
+sign_out_url_keycloak = (
+    "http://localhost:8080/realms/myrealm/protocol/openid-connect/logout"
+)
 sign_out_url_oauth_proxy_encoded = urllib.parse.quote(sign_out_url_oauth_proxy)
 
-# Use Keycloak sign out page, then make Keycloak redirect to oauth proxy sign out page 
+# Use Keycloak sign out page, then make Keycloak redirect to oauth proxy sign out page
 # to clear oauth proxy's cookie
 sign_out_url_oauth_proxy_and_keycloak = f"{sign_out_url_keycloak}?post_logout_redirect_uri={sign_out_url_oauth_proxy_encoded}&client_id=myclient"
 
 with st.sidebar:
     st.write(
-        f'<a href="{sign_out_url_oauth_proxy_and_keycloak}" target="_self">Logout</a>', 
-        unsafe_allow_html=True
+        f'<a href="{sign_out_url_oauth_proxy_and_keycloak}" target="_self">Logout</a>',
+        unsafe_allow_html=True,
     )
 
 st.write("Decoded JWT passed from oauth2-proxy in request header:")
-payload = jwt.decode(
-    access_token.encode("utf-8"),
-    options={"verify_signature": False})
+payload = jwt.decode(access_token.encode("utf-8"), options={"verify_signature": False})
 st.write(payload)
